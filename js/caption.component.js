4411f2c9557c2afed6acf8f5b825c697dbf7a6e2
Lena Ripple <ripplelena@gmail.com>
2017-04-19 20:14:14 -0600

    caption page

--- js/caption.component.js
+++ js/caption.component.js
@@ -5,5 +5,7 @@ angular
 function CaptionController() {
   const vm = this
 
-  vm.$onInit = function() {}
+  vm.$onInit = function() {
+    vm.image = "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRAJj7pECgmRZdyt_zomQIVboP_q0prP2s28OyVwSi4iyft7haFEyVrU8E"
+  }
 }
